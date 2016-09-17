48836803ad2fb3523b920e7959100de3173dba66
kakakaya <kakakaya+git@gmail.com>
2016-09-18 04:48:27 +0900

    new optional argument: add-url

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ Goから診断メーカー（ <https://shindanmaker.com> ）へのアクセス�
 
 # Usage
 `% goshindan -s 509717 -u kakakaya`
+
+## 引数
 * `-s` 診断メーカーのID。必須。
 * `-u` ユーザー名(診断したい名前を入れて下さいの部分)。必須ではない。
+* `--append-url` 診断メーカーのURLを末尾に付加して出力する。
--- goshindan.go
+++ goshindan.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/kakakaya/goshindan/utils"
 	"github.com/urfave/cli"
@@ -25,6 +26,10 @@ func main() {
 			Value: "Gopher a.k.a SandBag",
 			Usage: "診断したい名前を入れて下さい",
 		},
+		cli.BoolFlag{
+			Name:  "add-url",
+			Usage: "末尾に診断メーカーのURLを付加して出力する。",
+		},
 	}
 
 	app.Action = func(c *cli.Context) error {
@@ -32,6 +37,13 @@ func main() {
 			panic(fmt.Sprintf("shindanIDの値が不正です: %d\n", c.Int("shindanID")))
 		}
 		result, _ := utils.Shindan(c.Int("shindanID"), c.String("username"))
+		if c.Bool("add-url") {
+			if strings.Index(result, "\n") == -1 {
+				result = fmt.Sprintf("%s https://shindanmaker.com/%d", result, c.Int("shindanID"))
+			} else {
+				result = fmt.Sprintf("%s\nhttps://shindanmaker.com/%d\n", result, c.Int("shindanID"))
+			}
+		}
 		fmt.Println(result)
 		return nil
 	}
